6dd7694d45362e095f3e45d7f5ab4af8f85904d7
Kanta Oikawa <k.oikawa@kantacky.com>
2023-04-19 18:11:39 +0900

    Initial Commit

---
+++ TCA/TCAApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCAApp.swift
+//  TCA
+//
+//  Created by Kanta Oikawa on 2023/04/19.
+//
+
+import SwiftUI
+
+@main
+struct TCAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
